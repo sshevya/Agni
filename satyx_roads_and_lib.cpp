94add6876eefb997c5da88d61886d40507dd75d1
satyx <satyamcrj@gmail.com>
2019-12-10 23:58:06 +0530

    roads and lib

---
+++ satyx_roads_and_lib.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void BFS(int i,vector<vector<int> > *adj, vector<bool> *visited)
+{
+     
+    list<int> queue;
+    queue.push_back(i);
+
+    while(!queue.empty())
+    {
+        int x = queue.front();
+        (*visited)[x] = true;
+        queue.pop_front();
+        for(int z:(*adj)[x])
+        {
+            if(!(*visited)[z])
+            {
+                queue.push_back(z);
+                (*visited)[z] = true;
+            }
+        }        
+    }
+}
+
+int components(vector<vector<int> > *adj,int n)
+{
+    vector<bool> visited(n,false);
+    int count = 0;
+    for(int i=0;i<(*adj).size();i++)
+    {
+        
+        if(!visited[i])
+        {
+            count++;
+            BFS(i,adj,&visited);
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t-->0)
+    {
+        int n,m,c_r,c_l;
+        cin>>n>>m>>c_r>>c_l;
+        vector<int> parent(n,-1);
+        vector<int> key(n,INT_MAX);
+        vector<vector<int> > adj(n,vector<int> (0));
+        for(int i=0;i<m;i++)
+        {
+            int a,b;
+            cin>>a>>b;
+            adj[a-1].push_back(b-1);
+            adj[b-1].push_back(a-1);
+                                           
+        }
+	   
+        int comp = components(&adj,n);
+        cout<<c_r*(n-comp)+c_l*comp<<endl;
+    }
+	return 0;
+}
