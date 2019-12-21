a7547be4ec54d85a30efc7fba4e718257da18f0d
sshevya <37146723+sshevya@users.noreply.github.com>
2019-12-22 00:46:13 +0530

    Hackerrank JourneytoMoon
    To pass testcase 11  change int to long in main function also

--- st_journeytomoon_hackerrank.cpp
+++ st_journeytomoon_hackerrank.cpp
@@ -1,37 +1,135 @@
+
 #include <bits/stdc++.h>
 
 using namespace std;
-// Complete the substrings function below.
 
-long long substrings(string n) {
-    long long len=n.length();
-    long long num[len];
-    const long long M=1000000007;
-     // converts character to int
-    long long sum=0,f=1;
-    
-    for(long long i=len-1;i>=0;i--){
-    
-              num[i]=n[i]-'0';
-        sum+=(num[i]*f*len)%M;  
-        f=(f*10+1)%M;
-        len--;  
+vector<string> split_string(string);
+long mycount=0;
+void DFS(int v, bool visited[], vector<int> adj[]){
+   visited[v]=true;
+   mycount++;
+   vector<int>::iterator i;
+   for(i=adj[v].begin();i!=adj[v].end();i++){
+       if(!visited[*i]){
+           DFS(*i,visited,adj);
+       }
     }
-return sum%M;
+}   
+
+// Complete the journeyToMoon function below.
+long journeyToMoon(int n, vector<vector<int>> astronaut) {
+    int arr[n]={0};
+      int m= astronaut.size();
+    vector<int> adj[n];
+    for(int i=0;i<m;i++){
+         int u=astronaut[i][0];
+         int v=astronaut[i][1];
+         adj[u].push_back(v);
+         adj[v].push_back(u);
+         arr[u]=1;
+         arr[v]=1;
+    }
+
+    bool visited[n];
+    for(int i=0;i<n;i++){
+        if(arr[i]==1){
+            visited[i]=false;
+        }else{
+            visited[i]=true;
+        }
+    }
+    long curr=0,prev=0,p[n]={1},j=0,no_of_vertices=0,val=0;//p is an array to store no of adjacent vertices 
+    long M=1000000007;
+    for(int i=0;i<n;i++){
+        if(!visited[i]){
+             
+           DFS(i,visited,adj);
+         
+            curr=(mycount)%M;  // my count variable gives total no of vertices that has been visited after each iteration of DFS
+            
+            p[j]=curr-prev;
+            no_of_vertices=p[j]+prev;
+         
+            val+=(p[j]*prev)%M;    //a*b+(a+b)*c+....
+               prev=curr;
+            j++;
+        }
+        
+    }/*
+    while(n-mycount!=0){
+        curr=curr+1;
+        p[j]=(curr-prev)%M;
+        no_of_vertices+=p[j];
+        val+=((p[j])%M*(prev)%M)%M;    //a*b+(a+b)*c+....
+        prev=curr;
+        j++;
+        n--;
+        
+    }
+*/
+long l=n-mycount;
+val=val+(l)*no_of_vertices+l*(l-1)/2;
+return val;
 }
 
 int main()
 {
     ofstream fout(getenv("OUTPUT_PATH"));
 
-    string n;
-    getline(cin, n);
+    string np_temp;
+    getline(cin, np_temp);
 
-    long long result = substrings(n);
+    vector<string> np = split_string(np_temp);
+
+    int n = stoi(np[0]);
+
+    int p = stoi(np[1]);
+
+    vector<vector<int>> astronaut(p);
+    for (int i = 0; i < p; i++) {
+        astronaut[i].resize(2);
+
+        for (int j = 0; j < 2; j++) {
+            cin >> astronaut[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    long result = journeyToMoon(n, astronaut);
 
     fout << result << "\n";
 
     fout.close();
 
     return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
 }
