d3adf1d6677cae1a44a4be3c2922e9f0f5c53499
sshevya <37146723+sshevya@users.noreply.github.com>
2019-12-25 16:59:36 +0530

    LCS use static matrix(imp)

---
+++ st_commonchild_hackerrank.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+// Complete the commonChild function below.
+long commonChild(string s1, string s2) {
+    long m=s1.length();
+    long n=s2.length();
+    static long L[5001][5001];
+   
+    for(long i=0;i<=m;i++){
+        for(long j=0;j<=n;j++){
+            if(i==0 || j==0){
+                L[i][j]=0;
+           }
+            else if(s1[i-1]==s2[j-1]){
+                L[i][j]=1+L[i-1][j-1];
+
+            }else{
+                L[i][j]=max(L[i-1][j],L[i][j-1]);
+            }
+
+        }
+    }
+return L[m][n];
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s1;
+    getline(cin, s1);
+
+    string s2;
+    getline(cin, s2);
+
+    long result = commonChild(s1, s2);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
